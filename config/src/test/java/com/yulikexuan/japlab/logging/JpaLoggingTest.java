1b992528a3e9fbf9d66b0911807266e8ec12b581
yul <yu.li@tecsys.com>
2020-05-18 14:25:07 -0400

    Use c3p0 Connection Pool with Hibernate. Closes #3

--- config/src/test/java/com/yulikexuan/japlab/logging/JpaLoggingTest.java
+++ config/src/test/java/com/yulikexuan/japlab/logging/JpaLoggingTest.java
@@ -6,22 +6,24 @@ package com.yulikexuan.japlab.logging;
 
 import com.yulikexuan.japlab.AbstractTestCase;
 import com.yulikexuan.japlab.domain.Professor;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Test JPA Logging - ")
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class JpaLoggingTest extends AbstractTestCase  {
 
     @Test
-    public void test_Given_EntityManager_Then_Run_Transaction() {
+    @Order(1)
+    public void test_Given_Transaction_Then_Save_A_New_Entity() {
 
         // Given
         EntityTransaction transaction = this.entityManager.getTransaction();
@@ -42,6 +44,81 @@ public class JpaLoggingTest extends AbstractTestCase  {
                 this.getClass().getSimpleName());
 
         logger.info(">>>>>>> Logger's name is {}", this.logger.getName());
+    }
+
+    @Test
+    @Order(2)
+    void test_Given_Transaction_Then_Query_Existing_Entity() {
+
+        // Given
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        String queryString = "SELECT p FROM Professor p WHERE p.firstName = :fname";
+        TypedQuery<Professor> query = this.entityManager.createQuery(queryString,
+                Professor.class);
+        query.setParameter("fname", "Jane");
+
+        // When
+        Professor professor = query.getSingleResult();
+        transaction.commit();
+
+        // Then
+        assertThat(professor.getFirstName()).isEqualTo("Jane");
+        assertThat(professor.getLastName()).isEqualTo("Doe");
+    }
+
+    @Test
+    @Order(3)
+    void test_Given_Transaction_Then_Update_Existing_Entity() {
+
+        // Given
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        String queryString = "SELECT p FROM Professor p WHERE p.firstName = :fname";
+        TypedQuery<Professor> query = this.entityManager.createQuery(queryString,
+                Professor.class);
+        query.setParameter("fname", "Jane");
+
+        Professor professor = query.getSingleResult();
+
+        // When
+        professor.setFirstName("Steve");
+
+        transaction.commit();
+
+        // Then
+        assertThat(professor.getFirstName()).isEqualTo("Steve");
+        assertThat(professor.getLastName()).isEqualTo("Doe");
+    }
+
+    @Test
+    @Order(4)
+    void test_Given_Transaction_Then_Delete_Existing_Entity() {
+
+        // Given
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        String queryString = "SELECT p FROM Professor p WHERE p.firstName = :fname";
+        TypedQuery<Professor> query = this.entityManager.createQuery(queryString,
+                Professor.class);
+        query.setParameter("fname", "Steve");
+
+        Professor professor = query.getSingleResult();
+
+        // When
+        this.entityManager.remove(professor);
+
+        transaction.commit();
+
+        // Then
+        assertThat(professor.getFirstName()).isEqualTo("Steve");
+        assertThat(professor.getLastName()).isEqualTo("Doe");
     }
 
 }///:~
